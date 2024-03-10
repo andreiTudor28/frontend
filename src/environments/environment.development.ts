628312d1c43fceb80d75c993e5e5fd7498bcbd65
andreiTudor28 <118561116+andreiTudor28@users.noreply.github.com>
2024-03-10 12:46:18 +0200

    Add files via upload

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "MKS SHOP",
+  appOwner: "Tudor Andrei ",
+  appLogo: "https://key-shop.ro/img/master-key-shop-logo-1623483247.jpg",
+  itemCategories: ['SMALL', 'MEDIUM', 'LARGE']
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "MKS SHOP ",
+  appOwner: "Tudor Andrei",
+  appLogo: "https://www.creativefabrica.com/wp-content/uploads/2019/02/Online-shop-shopping-shop-logo-by-DEEMKA-STUDIO-3.jpg",
+  itemCategories: ['SMALL', 'MEDIUM', 'LARGE']
+};
